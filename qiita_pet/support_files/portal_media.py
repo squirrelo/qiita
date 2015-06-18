a68b78a85ba402bdc1c834030bdf6a95bb26d93a
Joshua Shorenstein <squirrelo@gmail.com>
2015-06-18 10:40:05 -0700

    add portal media file

---
+++ qiita_pet/support_files/portal_media.py
@@ -0,0 +1,19 @@
+_SITEBASE = {
+    'logo': "static/img/logo-clear.png",
+    'title': "Qiita",
+    'header': "<i>Qiita</i>: Spot Patterns",
+    'index_text': "",
+    'navbar_color': "#000000",
+    'navbar_text_color': "#999",
+    'navbar_text_highlight': "#fff",
+    'main_text_bg': "#eee"
+}
+
+_STUDY_LIST = {
+    'example_search': "env_matter = soil"
+}
+
+portal_styling = {
+    'sitebase': _SITEBASE,
+    'study_list': _STUDY_LIST
+}
